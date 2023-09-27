c1416528587d0eb5771928ecae4bad383f32b91f
oscarsotelo <oscaresotelo@gmail.com>
2023-09-27 12:28:23 -0300

    cuarta

--- tablero.py
+++ tablero.py
@@ -1,113 +1,29 @@
 
-# import streamlit as st
-# import sqlite3
-# import pandas as pd
-
-
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#                      .container {
-#                 display: flex;
-#             }
-#             .logo-text {
-#                 font-weight:700 !important;
-#                 font-size:30px !important;
-#                 color: black !important;
-#                 padding-top: 50px !important;
-#             }
-#             .logo-img {
-#                 float:right;
-#             }
-#             </style>
-#             """
-# st.markdown(hide_st_style, 
-
-#     unsafe_allow_html=True)
-
-# def local_css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-# local_css("estilos.css")
-
-# def highlight_red(val):
-#     return f'color: red'
-# # Configurar la aplicación de Streamlit
-
-
-# # Conexión a la base de datos SQLite
-# conn = sqlite3.connect('inventario.db')
-# cursor = conn.cursor()
-# if "ingreso" not in st.session_state:
-#       st.session_state.ingreso = ""
-
-
-# if st.session_state.ingreso == "":
-#     st.warning("Por favor Ingrese Correctamente")
-# else:
-# 	# Crear una función para obtener los registros de Inventario relacionados con Productos
-# 	def obtener_registros():
-# 	    cursor.execute("""
-# 	    SELECT  Inventario.Cantidad_Stock
-# 	           , Inventario.Faltante, Productos.Nombre , Inventario.Fecha_Actualizacion
-# 	    FROM Inventario
-# 	    INNER JOIN Productos ON Inventario.Codigo = Productos.Codigo
-# 	    WHERE Inventario.Faltante >= Inventario.Cantidad_Stock
-# 	    """)
-# 	    return cursor.fetchall()
-
-# 	# Crear la aplicación Streamlit
-# 	def main():
-# 	    st.markdown("<h1 style='text-align: center'>Tablero de Control</h1>", unsafe_allow_html=True)
-	    
-# 	    registros = obtener_registros()
-	    
-# 	    if not registros:
-# 	        st.warning('No se encontraron registros.')
-# 	    else:
-# 	        st.write('Registros de Productos en Falta:')
-	        
-# 	        # Mapear los nombres de las columnas
-# 	        column_names = ["Stock", "Mínimo", "Nombre", "Última Fecha"]
-	        
-# 	        # Crear un DataFrame de pandas para los registros
-# 	        df = pd.DataFrame(registros, columns=column_names)
-	        
-	       
-# 	        # Mostrar el DataFrame con estilo
-	        
-# 	        st.dataframe(df.style.applymap(highlight_red, subset=['Stock']))
-# 	    # Cerrar la conexión a la base de datos al finalizar
-# 	    conn.close()
-
-# 	if __name__ == '__main__':
-# 	    main()
-
 import streamlit as st
 import sqlite3
 import pandas as pd
+import io
+import base64
 
 hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-                     .container {
-                display: flex;
-            }
-            .logo-text {
-                font-weight:700 !important;
-                font-size:30px !important;
-                color: black !important;
-                padding-top: 50px !important;
-            }
-            .logo-img {
-                float:right;
-            }
-            </style>
-            """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+.container {
+    display: flex;
+}
+.logo-text {
+    font-weight: 700 !important;
+    font-size: 30px !important;
+    color: black !important;
+    padding-top: 50px !important;
+}
+.logo-img {
+    float: right;
+}
+</style>
+"""
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 def local_css(file_name):
@@ -150,41 +66,74 @@ else:
                 INNER JOIN Productos ON Inventario.Codigo = Productos.Codigo
                 WHERE Inventario.Faltante <= Inventario.Cantidad_Stock
             """)
-
-
         else:
             st.error("Error inesperado")
 
         return cursor.fetchall()
 
+    # Agregar una función para exportar los datos a un archivo Excel
+    def export_to_excel(df, condition):
+        if condition == "Faltante":
+            filename = "productos_faltantes.xlsx"
+        elif condition == "Stockeado":
+            filename = "productos_stockeados.xlsx"
+        else:
+            return None
+
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, sheet_name='Sheet1')
+
+        return buffer.getvalue()
+
+    # Función para crear un enlace de descarga para el archivo
+    def get_binary_file_downloader_html(bin_data, file_label='File'):
+        b64 = base64.b64encode(bin_data).decode()
+        custom_css = f""" 
+            <style>
+                .download-link {{
+                    background-color: #008CBA;
+                    color: #FFFFFF;
+                    padding: 8px 12px;
+                    border-radius: 4px;
+                    text-decoration: none;
+                    font-weight: bold;
+                    display: inline-block;
+                    margin-top: 10px;
+                }}
+                .download-link:hover {{
+                    background-color: #005f7f;
+                }}
+            </style>
+        """
+        dl_link = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_label}.xlsx" class="download-link">{file_label}</a>'
+        return f"{custom_css}{dl_link}"
+
     # Crear la aplicación Streamlit
     def main():
         # st.markdown("<h1 style='text-align: center'>Tablero de Control</h1>", unsafe_allow_html=True)
         st.title("Tablero de Control")
-        st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+        st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
         # Agregar un control para seleccionar la condición
         condicion = st.radio("Selecciona una condición:", ["Stockeado", "Faltante"])
         if condicion == "Stockeado":
 
-	        registros = obtener_registros(condicion)
+            registros = obtener_registros(condicion)
 
-	        if not registros:
-	            st.warning('No se encontraron registros.')
-	        else:
-	            st.write('Registros de Productos en Falta:')
+            if not registros:
+                st.warning('No se encontraron registros.')
+            else:
+                st.write('Registros de Productos en Falta:')
 
-	            # Mapear los nombres de las columnas
-	            column_names = ["Stock", "Mínimo", "Nombre", "Última Fecha"]
+                # Mapear los nombres de las columnas
+                column_names = ["Stock", "Mínimo", "Nombre", "Última Fecha"]
 
-	            # Crear un DataFrame de pandas para los registros
-	            df = pd.DataFrame(registros, columns=column_names)
+                # Crear un DataFrame de pandas para los registros
+                df = pd.DataFrame(registros, columns=column_names)
 
-	            # Mostrar el DataFrame con estilo
-	            st.dataframe(df.style.applymap(highlight_blue, subset=['Stock']))
-
-	        # Cerrar la conexión a la base de datos al finalizar
-	        conn.close()
+                # Mostrar el DataFrame con estilo
+                st.dataframe(df.style.applymap(highlight_blue, subset=['Stock']))
 
         else:
             registros = obtener_registros(condicion)
@@ -194,17 +143,28 @@ else:
             else:
                 st.write('Registros de Productos en Falta:')
 
-	            # Mapear los nombres de las columnas
+                # Mapear los nombres de las columnas
                 column_names = ["Stock", "Mínimo", "Nombre", "Última Fecha"]
 
-	            # Crear un DataFrame de pandas para los registros
+                # Crear un DataFrame de pandas para los registros
                 df = pd.DataFrame(registros, columns=column_names)
 
-	            # Mostrar el DataFrame con estilo
+                # Mostrar el DataFrame con estilo
                 st.dataframe(df.style.applymap(highlight_red, subset=['Stock']))
 
-	        # Cerrar la conexión a la base de datos al finalizar
-            conn.close()	
+        # Exportar a Excel
+        if st.button("Descargar Excel"):
+            buffer = export_to_excel(df, condicion)
+            if condicion == "Faltante":
+                filename = "productos_faltantes"
+            elif condicion == "Stockeado":   
+            	filename = "productos_stockeados"
+            else:
+            	filename = "archivo"
+            st.success(f"Archivo Excel generado con éxito.")
+            st.markdown(get_binary_file_downloader_html(buffer, filename), unsafe_allow_html=True)
+        # Cerrar la conexión a la base de datos al finalizar
+        conn.close()
 
     if __name__ == '__main__':
         main()
