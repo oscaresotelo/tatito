89969e2ef0407a213bfa6bc3aa5810f85817b153
oscarsotelo <oscaresotelo@gmail.com>
2023-10-02 07:10:19 -0300

    primeradellunes

--- completo.py
+++ completo.py
@@ -55,49 +55,125 @@ else:
         conn.close()
         return producto
 
-    def agregar_producto(codigo, nombre, descripcion, precio_compra, precio_venta, proveedor, unidad_medida):
+    def agregar_producto(codigo, nombre, descripcion, precio_compra, precio_venta, proveedor, unidad_medida,cantidad_medida):
         conn = sqlite3.connect("inventario.db")
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO Productos (Codigo, Nombre,  Descripcion, Precio_Compra, Precio_Venta, Proveedor, Unidad_Medida)
-            VALUES (?, ?, ?, ?, ?, ?,?)
-        """, (codigo, nombre,  descripcion, precio_compra, precio_venta, proveedor, unidad_medida))
+            INSERT INTO Productos (Codigo, Nombre,  Descripcion, Precio_Compra, Precio_Venta, Proveedor, Unidad_Medida, Cantidad_Medida)
+            VALUES (?, ?, ?, ?, ?, ?,?,?)
+        """, (codigo, nombre,  descripcion, precio_compra, precio_venta, proveedor, unidad_medida,cantidad_medida))
         conn.commit()
         conn.close()
 
+    # def agregar_movimiento_inventario(codigo, tipo_movimiento, cantidad_movida, usuario, razon_movimiento, fechavto):
+    #     conn = sqlite3.connect("inventario.db")
+    #     cursor = conn.cursor()
+    #     fecha_hora_movimiento = datetime.datetime.now()
+    #     cursor.execute("""
+    #         INSERT INTO Movimiento_Inventario (Codigo, Tipo_Movimiento, Cantidad_Movida, Fecha_Hora_Movimiento, Usuario, Razon_Movimiento,FechaVto)
+    #         VALUES (?, ?, ?, ?, ?, ?,?)
+    #     """, (codigo, tipo_movimiento, cantidad_movida, fecha_hora_movimiento, usuario, razon_movimiento,fechavto))
+    #     conn.commit()
+    #     conn.close()
+
+    #     # Actualizar la tabla "Inventario"
+    #     conn_inventario = sqlite3.connect("inventario.db")
+    #     cursor_inventario = conn_inventario.cursor()
+
+    #     # Buscar el código en la tabla "Inventario"
+    #     cursor_inventario.execute("SELECT * FROM Inventario WHERE Codigo=?", (codigo,))
+    #     row = cursor_inventario.fetchone()
+
+    #     if row is None:
+    #         # Si no existe el código, insertarlo en la tabla "Inventario"
+    #         cursor_inventario.execute("INSERT INTO Inventario (Codigo, Cantidad_Stock, Fecha_Actualizacion) VALUES (?, ?, ?)", (codigo, 0, datetime.datetime.now()))
+    #         conn_inventario.commit()
+
+    #     # Sumar al campo Cantidad_Stock si el Tipo_Movimiento es "Entrada"
+    #     if tipo_movimiento == "Entrada":
+    #         cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock + ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida, datetime.datetime.now(), codigo))
+    #         conn_inventario.commit()
+    #     if tipo_movimiento == "Salida":
+    #         cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock - ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida, datetime.datetime.now(), codigo))
+    #         conn_inventario.commit()
+
+    #     conn_inventario.close()
     def agregar_movimiento_inventario(codigo, tipo_movimiento, cantidad_movida, usuario, razon_movimiento, fechavto):
-        conn = sqlite3.connect("inventario.db")
-        cursor = conn.cursor()
-        fecha_hora_movimiento = datetime.datetime.now()
-        cursor.execute("""
-            INSERT INTO Movimiento_Inventario (Codigo, Tipo_Movimiento, Cantidad_Movida, Fecha_Hora_Movimiento, Usuario, Razon_Movimiento,FechaVto)
-            VALUES (?, ?, ?, ?, ?, ?,?)
-        """, (codigo, tipo_movimiento, cantidad_movida, fecha_hora_movimiento, usuario, razon_movimiento,fechavto))
-        conn.commit()
-        conn.close()
-
-        # Actualizar la tabla "Inventario"
-        conn_inventario = sqlite3.connect("inventario.db")
-        cursor_inventario = conn_inventario.cursor()
-
-        # Buscar el código en la tabla "Inventario"
-        cursor_inventario.execute("SELECT * FROM Inventario WHERE Codigo=?", (codigo,))
-        row = cursor_inventario.fetchone()
-
-        if row is None:
-            # Si no existe el código, insertarlo en la tabla "Inventario"
-            cursor_inventario.execute("INSERT INTO Inventario (Codigo, Cantidad_Stock, Fecha_Actualizacion) VALUES (?, ?, ?)", (codigo, 0, datetime.datetime.now()))
-            conn_inventario.commit()
-
-        # Sumar al campo Cantidad_Stock si el Tipo_Movimiento es "Entrada"
         if tipo_movimiento == "Entrada":
-            cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock + ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida, datetime.datetime.now(), codigo))
-            conn_inventario.commit()
-        if tipo_movimiento == "Salida":
-            cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock - ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida, datetime.datetime.now(), codigo))
-            conn_inventario.commit()
+            conn = sqlite3.connect("inventario.db")
+            cursor = conn.cursor()
+            fecha_hora_movimiento = datetime.datetime.now()
 
-        conn_inventario.close()
+            # Obtener el valor de Cantidad_Medida de la tabla "Productos" usando el código
+            cursor.execute("SELECT Cantidad_Medida FROM Productos WHERE Codigo=?", (codigo,))
+            cantidad_medida_producto = cursor.fetchone()[0]
+
+
+            # Calcular la cantidad movida multiplicando por Cantidad_Medida
+            cantidad_movida_total = cantidad_movida * cantidad_medida_producto
+
+            cursor.execute("""
+                INSERT INTO Movimiento_Inventario (Codigo, Tipo_Movimiento, Cantidad_Movida, Fecha_Hora_Movimiento, Usuario, Razon_Movimiento,FechaVto)
+                VALUES (?, ?, ?, ?, ?, ?,?)
+            """, (codigo, tipo_movimiento, cantidad_movida_total, fecha_hora_movimiento, usuario, razon_movimiento,fechavto))
+            conn.commit()
+            conn.close()
+
+            # Actualizar la tabla "Inventario"
+            conn_inventario = sqlite3.connect("inventario.db")
+            cursor_inventario = conn_inventario.cursor()
+
+            # Buscar el código en la tabla "Inventario"
+            cursor_inventario.execute("SELECT * FROM Inventario WHERE Codigo=?", (codigo,))
+            row = cursor_inventario.fetchone()
+
+            if row is None:
+                # Si no existe el código, insertarlo en la tabla "Inventario"
+                cursor_inventario.execute("INSERT INTO Inventario (Codigo, Cantidad_Stock, Fecha_Actualizacion) VALUES (?, ?, ?)", (codigo, 0, datetime.datetime.now()))
+                conn_inventario.commit()
+
+            # Sumar al campo Cantidad_Stock si el Tipo_Movimiento es "Entrada"
+            if tipo_movimiento == "Entrada":
+                cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock + ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida_total, datetime.datetime.now(), codigo))
+                conn_inventario.commit()
+            # if tipo_movimiento == "Salida":
+            #     cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock - ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida_total, datetime.datetime.now(), codigo))
+            #     conn_inventario.commit()
+
+            conn_inventario.close()
+        else:
+            conn = sqlite3.connect("inventario.db")
+            cursor = conn.cursor()
+            fecha_hora_movimiento = datetime.datetime.now()
+            cursor.execute("""
+                INSERT INTO Movimiento_Inventario (Codigo, Tipo_Movimiento, Cantidad_Movida, Fecha_Hora_Movimiento, Usuario, Razon_Movimiento,FechaVto)
+                VALUES (?, ?, ?, ?, ?, ?,?)
+            """, (codigo, tipo_movimiento, cantidad_movida, fecha_hora_movimiento, usuario, razon_movimiento,fechavto))
+            conn.commit()
+            conn.close()
+
+            # Actualizar la tabla "Inventario"
+            conn_inventario = sqlite3.connect("inventario.db")
+            cursor_inventario = conn_inventario.cursor()
+
+            # Buscar el código en la tabla "Inventario"
+            cursor_inventario.execute("SELECT * FROM Inventario WHERE Codigo=?", (codigo,))
+            row = cursor_inventario.fetchone()
+
+            if row is None:
+                # Si no existe el código, insertarlo en la tabla "Inventario"
+                cursor_inventario.execute("INSERT INTO Inventario (Codigo, Cantidad_Stock, Fecha_Actualizacion) VALUES (?, ?, ?)", (codigo, 0, datetime.datetime.now()))
+                conn_inventario.commit()
+
+            # Sumar al campo Cantidad_Stock si el Tipo_Movimiento es "Entrada"
+            # if tipo_movimiento == "Entrada":
+            #     cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock + ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida, datetime.datetime.now(), codigo))
+            #     conn_inventario.commit()
+            if tipo_movimiento == "Salida":
+                cursor_inventario.execute("UPDATE Inventario SET Cantidad_Stock = Cantidad_Stock - ?, Fecha_Actualizacion = ? WHERE Codigo = ?", (cantidad_movida, datetime.datetime.now(), codigo))
+                conn_inventario.commit()
+
+            conn_inventario.close()
 
     qr_code = qrcode_scanner(key='qrcode_scanner')
 
@@ -124,6 +200,7 @@ else:
         else:
             st.write(f"Producto con código {qr_code} no encontrado en la base de datos.")
             st.write("Agregar nuevo producto:")
+            st.write("Luego, leer Nuevamente Para Cargar Movimiento")
             nuevo_nombre = st.text_input("Nombre:")
             # nuevo_rubro = st.text_input("Rubro:")
             # nuevo_subrubro = st.text_input("Subrubro:")
@@ -133,10 +210,11 @@ else:
             nuevo_precio_venta = st.number_input("Precio de Venta:")
             nuevo_proveedor = st.text_input("Proveedor:")
             nueva_unidad_medida = st.text_input("Unidad de Medida:")
-            fecha_vencimiento = st.date_input("Ingrese Fecha Vencimiento")
+            nueva_cantidad_medida = st.number_input("Ingresar Cantidad")
+            #fecha_vencimiento = st.date_input("Ingrese Fecha Vencimiento")
             
             if st.button("Guardar Producto"):
-                agregar_producto(qr_code, nuevo_nombre,  nueva_descripcion, nuevo_precio_compra, nuevo_precio_venta, nuevo_proveedor, nueva_unidad_medida)
+                agregar_producto(qr_code, nuevo_nombre,  nueva_descripcion, nuevo_precio_compra, nuevo_precio_venta, nuevo_proveedor, nueva_unidad_medida,nueva_cantidad_medida)
                 st.success(f"Producto con código {qr_code} agregado correctamente.")
     else:
         st.write("Escanea un código QR para buscar un producto.")
