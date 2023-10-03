fde8793d5b89f61908a89b9ce9c726e0ad440b42
oscarsotelo <oscaresotelo@gmail.com>
2023-10-03 14:56:39 -0300

    cuartadelmartes

--- consulta.py
+++ consulta.py
@@ -1,4 +1,79 @@
 
+# import streamlit as st
+# import sqlite3
+# import pandas as pd
+# import plotly.express as px
+
+# hide_st_style = """
+#     <style>
+#     #MainMenu {visibility: hidden;}
+#     footer {visibility: hidden;}
+#     header {visibility: hidden;}
+#     .container {
+#         display: flex;
+#     }
+#     .logo-text {
+#         font-weight: 700 !important;
+#         font-size: 30px !important;
+#         color: black !important;
+#         padding-top: 50px !important;
+#     }
+#     .logo-img {
+#         float: right;
+#     }
+#     </style>
+# """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# local_css("estilos.css")
+
+# # Conexión a la base de datos SQLite
+# conn = sqlite3.connect('inventario.db')
+# cursor = conn.cursor()
+
+# # Función para realizar la consulta
+# def consulta_entre_fechas(fecha_inicio, fecha_fin, tipo_movimiento):
+#     cursor.execute("""
+#     SELECT mi.Tipo_Movimiento, mi.Cantidad_Movida,  p.Nombre, mi.Fecha_Hora_Movimiento, mi.Razon_Movimiento
+#     FROM Movimiento_Inventario mi
+#     JOIN Productos p ON mi.Codigo = p.Codigo
+#     WHERE mi.Fecha_Hora_Movimiento BETWEEN ? AND ? AND mi.Tipo_Movimiento = ?
+#     """, (fecha_inicio, fecha_fin, tipo_movimiento))
+#     data = cursor.fetchall()
+#     return data
+
+# # Encabezado de la aplicación
+# # st.markdown("<h1 style='text-align: center; color: red;'>Consulta de Stock</h1>", unsafe_allow_html=True)
+# st.title("Consulta de Stock")
+# st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+# # Formulario de consulta
+
+# fecha_inicio = st.date_input('Fecha de inicio')
+# fecha_fin = st.date_input('Fecha de fin')
+# tipo_movimiento = st.selectbox('Tipo de Movimiento', ['Entrada', 'Salida'])
+
+# if st.button('Consultar'):
+#     if fecha_inicio <= fecha_fin:
+#         data = consulta_entre_fechas(fecha_inicio, fecha_fin, tipo_movimiento)
+#         if len(data) > 0:
+#             st.write('Resultados de la consulta:')
+#             df = pd.DataFrame(data, columns=['Tipo_Movimiento', 'Cantidad_Movida', 'Nombre','Fecha_Hora_Movimiento', 'Razon_Movimiento'])
+#             df['Fecha_Hora_Movimiento'] = pd.to_datetime(df['Fecha_Hora_Movimiento'])
+#             df['Fecha_Hora_Movimiento'] = df['Fecha_Hora_Movimiento'].dt.strftime('%d/%m/%Y')  # Cambio del formato de la fecha
+#             st.dataframe(df)
+#             fig = px.bar(df, x='Fecha_Hora_Movimiento', y='Cantidad_Movida', title='Cantidad movida por fecha')
+#             st.plotly_chart(fig)
+#         else:
+#             st.warning('No se encontraron resultados para los filtros seleccionados.')
+#     else:
+#         st.error('La fecha de inicio debe ser anterior a la fecha de fin.')
+
+# # Cierre de la conexión a la base de datos
+# conn.close()
 import streamlit as st
 import sqlite3
 import pandas as pd
@@ -47,11 +122,10 @@ def consulta_entre_fechas(fecha_inicio, fecha_fin, tipo_movimiento):
     return data
 
 # Encabezado de la aplicación
-# st.markdown("<h1 style='text-align: center; color: red;'>Consulta de Stock</h1>", unsafe_allow_html=True)
 st.title("Consulta de Stock")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
-# Formulario de consulta
 
+# Formulario de consulta
 fecha_inicio = st.date_input('Fecha de inicio')
 fecha_fin = st.date_input('Fecha de fin')
 tipo_movimiento = st.selectbox('Tipo de Movimiento', ['Entrada', 'Salida'])
@@ -61,10 +135,16 @@ if st.button('Consultar'):
         data = consulta_entre_fechas(fecha_inicio, fecha_fin, tipo_movimiento)
         if len(data) > 0:
             st.write('Resultados de la consulta:')
-            df = pd.DataFrame(data, columns=['Tipo_Movimiento', 'Cantidad_Movida', 'Nombre','Fecha_Hora_Movimiento', 'Razon_Movimiento'])
+            df = pd.DataFrame(data, columns=['Tipo_Movimiento', 'Cantidad_Movida', 'Nombre', 'Fecha_Hora_Movimiento', 'Razon_Movimiento'])
             df['Fecha_Hora_Movimiento'] = pd.to_datetime(df['Fecha_Hora_Movimiento'])
             df['Fecha_Hora_Movimiento'] = df['Fecha_Hora_Movimiento'].dt.strftime('%d/%m/%Y')  # Cambio del formato de la fecha
             st.dataframe(df)
+
+            # Agrega esta parte para mostrar el DataFrame agrupado por "Nombre" y "Fecha_Hora_Movimiento"
+            st.subheader('Productos por Fecha y Nombre')
+            grouped_df = df.groupby(['Nombre', 'Fecha_Hora_Movimiento'])['Cantidad_Movida'].sum().reset_index()
+            st.dataframe(grouped_df)
+
             fig = px.bar(df, x='Fecha_Hora_Movimiento', y='Cantidad_Movida', title='Cantidad movida por fecha')
             st.plotly_chart(fig)
         else:
